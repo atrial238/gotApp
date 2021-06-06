32d7c86401c122ae1149ea172fa8ea15baeb1447
Mykhail Bobrykov <gelios238@gmail.com>
2021-06-06 11:41:47 +0300

    delete unnecessary components

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,9 @@
 export {default as ErrorMessage} from './ErrorMessage/ErrorMessage';
 export {default as Header} from './Header/Header';
-export {default as ItemDetails} from './ItemDetails/ItemDetails';
-export {default as ItemList} from './ItemList/ItemList';
-export {default as RandomChar} from './RandomChar/RandomChar';
-export {default as RowBlock} from './RowBlock/RowBlock';
-export {default as Spinner} from './Spinner/Spinner';
 export {default as NoMatch} from './NoMatch/NoMatch';
-export {default as Field} from './Field/Field';
 export {default as ListTitles} from './ListTitles/ListTitles';
 export {default as RandomItem} from './RandomItem/RandomItem';
 export {default as Preloader} from './Preloader/Preloader';
 export {default as ListCharacteristics} from './ListCharacteristics/ListCharacteristics';
 export {default as SpecificItem} from './SpecificItem/SpecificItem';
 export {default as TemplatePage} from './TemplatePage/TemplatePage';
-
