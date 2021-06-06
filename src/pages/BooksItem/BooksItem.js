b1a9605513ecf6ae1eb6802028f11847563de9f8
Mykhail Bobrykov <gelios238@gmail.com>
2021-06-06 10:59:52 +0300

    add backbutton

--- src/pages/BooksItem/BooksItem.js
+++ src/pages/BooksItem/BooksItem.js
@@ -1,13 +1,15 @@
 import React, {useEffect, useState} from 'react';
+import { useHistory, useParams } from 'react-router';
 
 import GetResource from '../../services/gotService';
 import {ErrorMessage, ListCharacteristics, Preloader} from '../../components'
-import { useParams } from 'react-router';
 import './booksItem.css';
+import backButton from '../../assets/img/backButton.svg';
 
 const BooksItem = () => {
 
 	const {id} = useParams();
+	const history = useHistory();
 
 	const {getBook} = new GetResource();
 	const [specificBook, setSpecificBook] = useState();
@@ -27,8 +29,11 @@ const BooksItem = () => {
 				: isError
 				? <ErrorMessage/>
 				: <div className='wrapper_specific_item'>
-					<h4>{specificBook.name}</h4>
+					<div className='title_wrapper'>
+						<div className='backbutton' onClick={() => history.goBack()}><img src={backButton} alt='backbutton'/></div>
+						<h4>{specificBook.name}</h4>
+					</div>
 					<ListCharacteristics specificItem={specificBook}/>
-				</div>
+					</div>
 }
 export default BooksItem;
